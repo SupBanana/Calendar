0cb9f0e87022afd8a27fba1beb4b570d397c6c90
Alexander <alex26600@ya.ru>
2022-03-17 16:47:00 +0600

    Initial Commit

---
+++ app/src/main/java/com/company/calendar/Events/AddEventActivity.java
@@ -0,0 +1,47 @@
+package com.company.calendar.Events;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.company.calendar.Calendars.CalendarUtils;
+import com.company.calendar.R;
+
+import java.time.LocalTime;
+
+public class AddEventActivity extends AppCompatActivity
+{
+    private EditText eventNameET;
+    private TextView eventDateTV, eventTimeTV;
+
+    private LocalTime time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+        initWidgets();
+        time = LocalTime.now();
+        eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
+        eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+    }
+
+    private void initWidgets()
+    {
+        eventNameET = findViewById(R.id.eventNameET);
+        eventDateTV = findViewById(R.id.eventDateTV);
+        eventTimeTV = findViewById(R.id.eventTimeTV);
+    }
+
+    public void saveEventAction(View view)
+    {
+        String eventName = eventNameET.getText().toString();
+        Event newEvent = new Event(eventName, CalendarUtils.selectedDate, time);
+        Event.eventsList.add(newEvent);
+        finish();
+    }
+}
